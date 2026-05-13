a605ce918dbfc7b981f544dd4adf0ed9e3acbf6e
bradleymaina <bradleymaina@proton.me>
2026-05-13 17:39:56 +0300

    Add persistance to script

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 import os
 
-command = input("Enter command: ").lower()
+while True : 
 
-if command == "nemo" : #nemo is the wake up call for the AI assistant
-    print("Hello Engineer Bradley")
+    command = input("Enter command: ").lower()
 
-elif command == "nemo browser" :
-    os.system("brave")
+    if command == "nemo" : #nemo is the wake up call for the AI assistant
+        print("Hello Engineer Bradley")
+
+    elif command == "nemo browser" :
+        os.system("brave")
 
 # specific for my file manager 
 # replace with appropriate file manager eg thunar , nautilus
-elif command == "nemo files" : 
-    os.system("dolphin")
+    elif command == "nemo files" : 
+        os.system("dolphin")
 
 #specific for my zsh
 #replace with appropriate shell manager 
-elif command == "nemo shell":
-    os.system("kitty") 
+    elif command == "nemo shell":
+        os.system("kitty") 
 
-elif command == "nemo code" :
-    os.system("antigravity")
+    elif command == "nemo code" :
+        os.system("antigravity")
  
-elif command == "nemo spotify" :
-    os.system("spotify")
+    elif command == "nemo spotify" :
+        os.system("spotify")
 
-else: 
-    print("Unknown command")
+    else: 
+        print("Unknown command")
